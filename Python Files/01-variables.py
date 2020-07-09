4b46daa0320b2d4b025f44d1baa34a460ff81b89
DerryPlaysXd <derryplays@gmail.com>
2020-07-09 03:40:49 +0200

    Added the '01-variables.py' file

---
+++ Python Files/01-variables.py
@@ -0,0 +1,21 @@
+"""
+Welcome to the first course of the Learn-Python-Basics project!
+
+Here you will learn how to create a new variable, print the variable out and do some basic math!
+"""
+
+#First of all let's create a variable:
+aVar = 3
+
+#We can print aVar out with the following line:
+print(aVar)
+
+#We can also do a simple user input into another variable, the user will be prompted to input a number
+aVar = int(input("Input a number:   "))
+#Once again we can print the variable out:
+print(aVar)
+
+
+"""
+This is the end of the course! Move on to another one!
+"""
