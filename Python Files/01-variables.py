cc715df18e49d20906891068c366d5904db8d7a9
DerryPlaysXd <derryplays@gmail.com>
2020-07-09 15:33:29 +0200

    Updated the '01-variables.py' file

--- Python Files/01-variables.py
+++ Python Files/01-variables.py
@@ -4,16 +4,19 @@ Welcome to the first course of the Learn-Python-Basics project!
 Here you will learn how to create a new variable, print the variable out and do some basic math!
 """
 
-#First of all let's create a variable:
+# First of all let's create a variable:
 aVar = 3
 
-#We can print aVar out with the following line:
+# We can print aVar out with the following line:
 print(aVar)
 
-#We can also do a simple user input into another variable, the user will be prompted to input a number
-aVar = int(input("Input a number:   "))
-#Once again we can print the variable out:
-print(aVar)
+# We can also do some calculations using variables
+aVar = 3
+# We need to define a second variable
+bVar = 2
+resultVar = aVar + bVar
+# Once again we can print the result out 3+2 = 5, so the output will be 5
+print(resultVar)
 
 
 """
