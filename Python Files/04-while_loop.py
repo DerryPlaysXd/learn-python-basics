7e5bed425755598d4b4f2570b865d6c5d485e9d5
Freskot <Z16APlasil@seznam.cz>
2020-07-16 13:05:36 +0200

    added 4. lesson

---
+++ Python Files/04-while_loop.py
@@ -0,0 +1,26 @@
+"""
+Welcome to the fourth course of the Learn-Python-Basics project!
+
+Here you will learn how to create and use while loop!
+"""
+
+# First we need to declare a variable for a while loop, which defines amount of repeating a code below.
+count = 0
+
+# This code is a start of while loop. The template is "while STATEMENT:". While the "STATEMENT" is equal to "True", code below is repeating.
+while count < 5:
+
+# This code print a short message for user about successfull repeating of code in WHILE block.
+# In this case, varible "count" doesn't need to convert to string, because it's separated by comma from string "Text".
+	print("Text", count+1)
+
+# This is the same as "count = count + 1". It's shorter and faster option for increasing a varible by 1.
+# Without this line of code, while loop goes to infinity.
+	count += 1
+
+# And finally print for check, what we executed.
+print("END!")
+
+"""
+This is the end of the course! Move on to another one!
+"""
